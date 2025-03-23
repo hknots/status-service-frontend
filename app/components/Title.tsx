791dbcc50356d413386f42dfd21bf78166123372
Hknots <hknots99@gmail.com>
2025-03-23 10:23:57 +0100

    refactor: set icon through props

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -1,30 +1,24 @@
 import {Box, Heading, HStack, VStack} from "@navikt/ds-react";
-import {GlassesIcon} from "@navikt/aksel-icons";
 
 interface TitleProps {
-  title: string
+  title: string;
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
 }
 
-export default function Title({ title }: TitleProps) {
-    return (
-      <Box
-        borderWidth="0 0 3 0"
-        borderColor="border-strong"
-        className="w-full"
-        paddingBlock="12 2"
-      >
-        <HStack gap="2">
-          <VStack
-            justify="center"
-          >
-            <GlassesIcon fontSize="2.5rem"/>
-          </VStack>
-          <Heading
-            size="xlarge"
-          >
-            {title}
-          </Heading>
-        </HStack>
-      </Box>
-    )
+export default function Title({title, icon: Icon}: TitleProps) {
+  return (
+    <Box
+      borderWidth="0 0 3 0"
+      borderColor="border-strong"
+      className="w-full"
+      paddingBlock="12 2"
+    >
+      <HStack gap="2">
+        <VStack justify="center">
+          <Icon fontSize="2.5rem"/>
+        </VStack>
+        <Heading size="xlarge">{title}</Heading>
+      </HStack>
+    </Box>
+  );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,16 @@
 import Title from "~/components/Title";
 import {VStack} from "@navikt/ds-react";
+import {GlassesIcon} from "@navikt/aksel-icons";
 
 export default function Index() {
   return (
     <VStack
       className="w-full h-full"
     >
-      <Title title="Miljø Oversikt - Api"/>
+      <Title
+        title="Miljø Oversikt - Api"
+        icon={GlassesIcon}
+      />
     </VStack>
   );
 }
